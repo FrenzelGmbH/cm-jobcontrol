674e2264340b6e9ec823525abd3957b8c5d9cbd0
Philipp Frenzel <philipp@frenzel.net>
2017-04-07 10:17:59 +0200

    Initial upload module structure
    init module

---
+++ Module.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * @company Frenzel GmbH
+ * @author Philipp Frenzel  
+ * @license MIT
+ * @version 0.1
+ */
+
+namespace net\frenzel\jobcontrol;
+
+class Module extends \yii\base\Module
+{
+	public function init()
+	{
+		parent::init();
+	}
+}
